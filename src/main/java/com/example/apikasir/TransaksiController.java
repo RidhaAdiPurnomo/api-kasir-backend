39beb814820445f9f5348f468ad6921b73154796
RidhaAdiPurnomo <ridha.adi@mhs.itenas.ac.id>
2026-07-05 23:27:03 +0700

    "refactor: memisahkan logika diskon ke service layer"

--- src/main/java/com/example/apikasir/TransaksiController.java
+++ src/main/java/com/example/apikasir/TransaksiController.java
@@ -1,5 +1,6 @@
 package com.example.apikasir;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,14 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TransaksiController {
 
+    // Menyuntikkan (Inject) TransaksiService ke dalam Controller
+    @Autowired
+    private TransaksiService transaksiService;
+
     @GetMapping("/api/transaksi/diskon")
     public String hitungDiskon(@RequestParam int totalBelanja) {
-        if (totalBelanja >= 100000) {
-            int diskon = totalBelanja * 10 / 100;
-            int totalBayar = totalBelanja - diskon;
-            return "Selamat, dapat diskon 10%! Total bayar: Rp " + totalBayar;
-        } else {
-            return "Tidak ada diskon. Total bayar: Rp " + totalBelanja;
-        }
+        // Controller sekarang hanya memanggil fungsi dari Service
+        return transaksiService.hitungDiskon(totalBelanja);
     }
 }
